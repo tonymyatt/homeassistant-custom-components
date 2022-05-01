85b4f0003a12fced99aa668ebe103017b34d2d37
tonymyatt <64132879+tonymyatt@users.noreply.github.com>
2022-05-01 22:36:26 +1000

    Update tuya_ir.py

--- sandbox/tuya_ir.py
+++ sandbox/tuya_ir.py
@@ -5,9 +5,9 @@ import sys
 
 #logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
-d = tinytuya.Device('108117185002916e0653', '192.168.1.224', 'fd1f63df5ff2eeb9')
+d = tinytuya.Device('10811653', '192', 'fff2eeb9')
 d.set_version(3.3)
 data = d.status() 
 print('Device status: %r' % data)
-data = d.status_sub_device('108117185002916e0653', 'bf6c4faebf0cab6ec7gs5m') 
+data = d.status_sub_device('108117183', 'bf6c4fags5m') 
 print('Device status: %r' % data)
